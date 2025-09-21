91c3dace1a289e5f255d00ceeb175f553cc545ed
Manasseh <believemanasseh@gmail.com>
2025-09-22 00:27:10 +0100

    feat: add text context

--- app/contexts.ts
+++ app/contexts.ts
@@ -2,3 +2,6 @@ import { createContext } from "react";
 import { TextLayer } from "./types";
 
 export const LayersContext = createContext<TextLayer[]>([]);
+export const TextContext = createContext<
+  [string | undefined, (newText: string) => void]
+>([undefined, () => {}]);
