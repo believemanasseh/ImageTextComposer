409f3276c50315eea8ba41b8a5cb0d7d74945df2
Manasseh <believemanasseh@gmail.com>
2025-09-23 21:56:39 +0100

    feat: modify text layer sidebar

--- app/components/LayerList.tsx
+++ app/components/LayerList.tsx
@@ -1,27 +1,33 @@
 "use client";
 
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import CustomisationModal from "./CustomisationModal";
 import { LayersContext } from "../contexts";
 
 export default function LayerList() {
   const [layers, setLayers] = useContext(LayersContext);
-  const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
+  const handleClick = (id: number) => {
+    setLayers(
+      layers.map((layer) =>
+        layer.id === id ? { ...layer, isOpen: true } : layer
+      )
+    );
   };
 
   return (
     <div>
-      <h2 className="font-bold mb-2">Layers</h2>
+      <h2 className="font-bold mb-2 text-center">Layers</h2>
       <ul className="leading-[2]">
         {layers.map((layer, index) => (
           <React.Fragment key={index}>
-            <li className="cursor-pointer" onClick={handleClick}>
+            <li
+              className="cursor-pointer hover:bg-blue-500 hover:text-white hover:rounded-md px-2 my-2"
+              onClick={() => handleClick(layer.id)}
+            >
               {layer.text}
             </li>
-            <CustomisationModal layer={layer} open={open} setOpen={setOpen} />
+            <CustomisationModal layer={layer} open={layer.isOpen} />
           </React.Fragment>
         ))}
       </ul>
