4737ff3110276cf090e3e96a745dc9fbc9277e0a
Manasseh <believemanasseh@gmail.com>
2025-09-27 05:13:48 +0100

    refactor: remove google fonts imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Kavivanar } from "next/font/google";
 import "./globals.css";
 import "@ant-design/v5-patch-for-react-19";
 import "toastr/build/toastr.min.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const kavivanar = Kavivanar({
-  variable: "--font-kavivanar",
-  subsets: ["latin"],
-  weight: ["400"],
-});
 
 export const metadata: Metadata = {
   title: "Image Text Composer",
@@ -33,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${kavivanar.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
